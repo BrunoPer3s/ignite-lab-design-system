aee2c70d4c4aff4d4c3588c38f1150ab248a482c
BrunoPer3s <brunoperes7@hotmail.com>
2022-10-13 16:48:58 -0300

    radix-ui/slot added and Text component finished

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,6 +6,15 @@ export default {
   component: Text,
   args: {
     children: 'Lorem ipsum.',
+    size: 'md'
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
   }
 } as Meta<TextProps>
 
@@ -22,3 +31,19 @@ export const Large: StoryObj<TextProps> = {
     size: 'lg'
   }
 };
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>testando</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}
